792d1e5556734442cf08107a938e3660860f18bc
Parth Thakkar <thakkarparth007@gmail.com>
2015-09-19 23:10:38 +0530

    Add meta tags. Fix protocol-issue. Rename login page

--- routes/logout.js
+++ routes/logout.js
@@ -3,20 +3,8 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/logout', function(req,res,next) {
-	function log(uname,dept) {
-		fs.open('./logs/login_log.txt','a',function(err, fd) {
-			var buf = new Buffer("[ " + new Date() + " ] LOGOUT: " + uname + "@" + dept + "\n");
-			fs.write(fd, buf, 0, buf.length, null, function(err) {
-				if(err)	throw err;
-				fs.close(fd, function(err) {
-					if(err)	throw err;
-				});
-			});
-		});
-	}
-	log(req.session.username, req.session.department);
 	req.session.destroy();
-	res.redirect('/');
+	res.redirect('/snapchallenge');
 	res.end();
 });
 
